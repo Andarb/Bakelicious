1442f6d7f352494bccd27c0c898c70b5180d35ee
Andarb <dev@andarb.com>
2018-05-11 21:46:38 +0100

    Add media player to the detailed steps

--- app/src/main/java/com/github/andarb/bakelicious/StepDetailsFragment.java
+++ app/src/main/java/com/github/andarb/bakelicious/StepDetailsFragment.java
@@ -1,7 +1,9 @@
 package com.github.andarb.bakelicious;
 
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,6 +12,20 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.github.andarb.bakelicious.data.Recipe;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,8 +39,11 @@ public class StepDetailsFragment extends Fragment {
 
     @BindView(R.id.step_description_text_view)
     TextView stepDescriptionTV;
+    @BindView(R.id.exoplayer_view)
+    PlayerView mPlayerView;
 
     private Recipe mRecipe;
+    private SimpleExoPlayer mPlayer;
     private Unbinder mButterknifeUnbinder;
 
     // Required empty public constructor
@@ -43,12 +62,23 @@ public class StepDetailsFragment extends Fragment {
         return f;
     }
 
-
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.step_details, container, false);
         mButterknifeUnbinder = ButterKnife.bind(this, view);
+
+        // 1. Create a default TrackSelector
+        Handler mainHandler = new Handler();
+        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        TrackSelection.Factory videoTrackSelectionFactory =
+                new AdaptiveTrackSelection.Factory(bandwidthMeter);
+        TrackSelector trackSelector =
+                new DefaultTrackSelector(videoTrackSelectionFactory);
+
+        // 2. Create the player and set its view
+        mPlayer = ExoPlayerFactory.newSimpleInstance(getActivity(), trackSelector);
+        mPlayerView.setPlayer(mPlayer);
 
         // Retrieve recipe from our activity, and populate details
         mRecipe = getArguments().getParcelable(InstructionsFragmentActivity.RECIPE_EXTRA);
@@ -59,7 +89,17 @@ public class StepDetailsFragment extends Fragment {
     }
 
     public void updateDetails(int step) {
-        //TODO update exoplayer here
+        String videoUrl = mRecipe.getSteps().get(step).getVideoURL();
+        if (!videoUrl.isEmpty()) {
+            // Prepare media player source
+            DefaultBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+            DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(getActivity(),
+                    Util.getUserAgent(getActivity(), getString(R.string.app_name)), bandwidthMeter);
+            MediaSource videoSource = new ExtractorMediaSource.Factory(dataSourceFactory)
+                    .createMediaSource(Uri.parse(videoUrl));
+            mPlayer.prepare(videoSource);
+        }
+
         stepDescriptionTV.setText(mRecipe.getSteps().get(step).getDescription());
     }
 
