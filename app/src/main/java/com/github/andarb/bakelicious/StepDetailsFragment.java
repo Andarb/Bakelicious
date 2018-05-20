aebfa1c8ee986550fa38b596475931970e239afa
Andarb <dev@andarb.com>
2018-05-20 19:32:27 +0100

    Fix fullscreen support for ExoPlayer

--- app/src/main/java/com/github/andarb/bakelicious/StepDetailsFragment.java
+++ app/src/main/java/com/github/andarb/bakelicious/StepDetailsFragment.java
@@ -13,11 +13,9 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -196,12 +194,18 @@ public class StepDetailsFragment extends Fragment {
                     mPlayerView.getLayoutParams();
             layoutParams.width = ConstraintLayout.LayoutParams.MATCH_PARENT;
             layoutParams.height = ConstraintLayout.LayoutParams.MATCH_PARENT;
+            layoutParams.bottomToBottom = ConstraintLayout.LayoutParams.PARENT_ID;
             mPlayerView.setLayoutParams(layoutParams);
 
-            // Hide notification and action bars
-            getActivity().getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
-            ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
+            // Hide system UI
+            View decorView = getActivity().getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_IMMERSIVE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN);
 
             // Hide navigation buttons
             mPreviousButton.setVisibility(View.GONE);
